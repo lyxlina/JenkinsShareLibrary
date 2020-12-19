0d4dc2b4637895d0e0b7e05b51319ed889d5218e
lyxlina <18513278472@163.com>
2020-12-19 18:08:27 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,12 +1,12 @@
 package org.devops
 
 //打印内容,格式化输出
-def printColor(color){
+def printMes(value,color){
     //println(content)
     colors = [
-        'blue' : "\033[30m 黑色字 \033[0m"，
-        'red' : "\033[31m 红色字 \033[0m"，
-        'yellow' : ""\033[33m 黄色字 \033[0m" ]
+        'blue' : "\033[30m ${value} \033[0m"，
+        'red' : "\033[31m ${value} \033[0m"，
+        'yellow' : ""\033[33m ${value} \033[0m" ]
     ansiColor('xterm') {
     // some block
         println(colors[color])
