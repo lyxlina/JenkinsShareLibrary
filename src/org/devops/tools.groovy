a9ee44aa41e25e772c6ed3dd79ba0bb3343b999d
lyxlina <18513278472@163.com>
2020-12-19 16:56:44 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,13 +1,13 @@
 package org.devops
 
-//打印内容
-def printMes(content){
+//打印内容,格式化输出
+def printMes(value,color){
     println(content)
     colors = [
-        'blue' : "\033[46;30m 天蓝底黑字 \033[0m"，
-        'red' : "\033[5;34m 红字在闪烁 \033[0m"，
-        'black' : "\033[30m 黑色字 \033[0m"，
-        'green' : "\033[32m 绿色字 \033[0m" ]
+        'blue' : "\033[46;30m ${value} \033[0m"，
+        'red' : "\033[5;34m ${value} \033[0m"，
+        'black' : "\033[30m ${value} \033[0m"，
+        'green' : "\033[32m ${value} \033[0m" ]
     ansiColor('xterm') {
     // some block
         println(colors[color])
