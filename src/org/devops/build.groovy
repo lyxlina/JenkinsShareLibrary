21fc900e33dc47af0bf55fed3edc3cc9cceea985
lyxlina <18513278472@163.com>
2020-12-22 10:41:20 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -4,7 +4,7 @@ package org.devops
 //构建类型
 def build(buildType,buildShell){
 
-    def buildTools = ["mvn":"M2","gradle":"GRADLE","npm":"NPM"]
+    def buildTools = ["mvn":"M3","gradle":"GRADLE","npm":"NPM"]
     println("当前的构建类型${buildType}")
     buildHome = buildTools[buildType]
     sh "${buildHome}/bin/${buildType} ${buildShell}"
